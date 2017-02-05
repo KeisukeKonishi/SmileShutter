58a35e41335e140d1944d7a9a774af66abdb4622
konishi <konishi87bid@gmail.com>
2017-02-05 19:45:38 +0900

    Initial Commit

---
+++ SmileShutter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmileShutter
+//
+//  Created by LUCKY on 2017/02/05.
+//  Copyright © 2017年 LUCKY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
